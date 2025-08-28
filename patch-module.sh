4e26c31fa2caae2a1eb65eaaf114ca12fec12e74
phantom111111222222 <tuphandhn3dtjxt@gmail.com>
2025-08-28 07:26:00 +0000

    fix: try use gsed in macos

--- patch-module.sh
+++ patch-module.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-SED=$(command -v gsed || echo sed)
+if command -v gsed &> /dev/null; then
+    SED_CMD="gsed"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    SED_CMD="sed -i ''"
+else
+    SED_CMD="sed -i"
+fi
 
-find . -name "*.go" -type f -exec $SED -i 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
-find . -name "Makefile" -type f -exec $SED -i 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
-find . -name "*.go" -type f -exec $SED -i 's|github\.com/sagernet/sing-box"|github.com/phantomcloude/core"|g' {} +
-find . -name "*.mod" -type f -exec $SED -i 's|github\.com/sagernet/sing-box|github.com/phantomcloude/core|g' {} +
+find . -name "*.go" -type f -exec $SED_CMD 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
+find . -name "Makefile" -type f -exec $SED_CMD 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
+find . -name "*.go" -type f -exec $SED_CMD 's|github\.com/sagernet/sing-box"|github.com/phantomcloude/core"|g' {} +
+find . -name "*.mod" -type f -exec $SED_CMD 's|github\.com/sagernet/sing-box|github.com/phantomcloude/core|g' {} +
