2cb9f9e3591d34c46273dd19225b8a8f688e73f9
phantom111111222222 <tuphandhn3dtjxt@gmail.com>
2025-08-28 07:22:50 +0000

    fix: try use gsed in macos

--- patch-module.sh
+++ patch-module.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-find . -name "*.go" -type f -exec sed -i 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
-find . -name "Makefile" -type f -exec sed -i 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
-find . -name "*.go" -type f -exec sed -i 's|github\.com/sagernet/sing-box"|github.com/phantomcloude/core"|g' {} +
-find . -name "*.mod" -type f -exec sed -i 's|github\.com/sagernet/sing-box|github.com/phantomcloude/core|g' {} +
+SED=$(command -v gsed || echo sed)
+
+find . -name "*.go" -type f -exec $SED -i 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
+find . -name "Makefile" -type f -exec $SED -i 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
+find . -name "*.go" -type f -exec $SED -i 's|github\.com/sagernet/sing-box"|github.com/phantomcloude/core"|g' {} +
+find . -name "*.mod" -type f -exec $SED -i 's|github\.com/sagernet/sing-box|github.com/phantomcloude/core|g' {} +
