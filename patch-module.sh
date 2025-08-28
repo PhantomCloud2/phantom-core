e7a74ecf0b8f7c1b2b744c6235c42b9c5c8e0109
phantom111111222222 <tuphandhn3dtjxt@gmail.com>
2025-08-28 15:05:05 +0000

    fix: proto regen

--- patch-module.sh
+++ patch-module.sh
@@ -11,7 +11,20 @@ else
     SED_CMD="sed -i"
 fi
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if ! command -v protoc &> /dev/null; then
+        brew install protobuf
+    fi
+else
+    if ! command -v protoc &> /dev/null; then
+        apt install -y protobuf-compiler
+    fi
+fi
+
 find . -name "*.go" -type f -exec $SED_CMD 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
+find . -name "*.proto" -type f -exec $SED_CMD 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
 find . -name "Makefile" -type f -exec $SED_CMD 's|github\.com/sagernet/sing-box/|github.com/phantomcloude/core/|g' {} +
 find . -name "*.go" -type f -exec $SED_CMD 's|github\.com/sagernet/sing-box"|github.com/phantomcloude/core"|g' {} +
 find . -name "*.mod" -type f -exec $SED_CMD 's|github\.com/sagernet/sing-box|github.com/phantomcloude/core|g' {} +
+
+(cd core && make proto_install && make fmt_install && make proto)
